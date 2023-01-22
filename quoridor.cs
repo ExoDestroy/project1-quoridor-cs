327ae6fae2a1681a700edb415decc5e6033e2ec2
ExoDestroy <juliansomi06@gmail.com>
2023-01-21 19:38:46 -0600

    Actual Quoridor 1.0

--- quoridor.cs
+++ quoridor.cs
@@ -159,8 +159,8 @@ class Quoridor
                 write(players[turns % players.Count()].PrintName);
                 Console.WriteLine();
 
-                foreach (String s in options)
-                    write(s);
+                for (byte i = 0; i < options.Count(); i++)
+                    write($"{i + 1} ). {options[i]}");
 
                 string? inp = Console.ReadLine();
 
@@ -170,67 +170,90 @@ class Quoridor
                     continue;
                 }
 
-                // Acceptable inputs
-                switch (inp.ToLower())
-                {   
-                    case "l":
-                    case "le":
-                    case "lef":
-                    case "left":
-                    case "move l":
-                    case "move le":
-                    case "move lef":
-                    case "move left":
-                        inp = "left";
-                        break;
-                    case "r":
-                    case "ri":
-                    case "rig":
-                    case "righ":
-                    case "right":
-                    case "move r":
-                    case "move ri":
-                    case "move rig":
-                    case "move righ":
-                    case "move right":
-                        inp = "right";
-                        break;
-                    case "u":
-                    case "up":
-                    case "move u":
-                    case "move up":
-                        inp = "up";
-                        break;
-                    case "d":
-                    case "do":
-                    case "dow":
-                    case "down":
-                    case "move d":
-                    case "move do":
-                    case "move dow":
-                    case "move down":
-                        inp = "down";
-                        break;
-                    case "w":
-                    case "wa":
-                    case "wal":
-                    case "wall":
-                    case "p":
-                    case "pl":
-                    case "pla":
-                    case "plac":
-                    case "place":
-                    case "place w":
-                    case "place wa":
-                    case "place wal":
-                    case "place wall":
-                        inp = "wall";
-                        break;
-                    default:
-                        write("Enter one of the given options:");
-                        continue;
-                }
+                int numInp;
+                
+                bool isInt = int.TryParse(inp, out numInp);
 
+                if (isInt && numInp > 0 && numInp <= options.Count())
+                {
+                    inp = options[numInp - 1];
+
+                    if (inp.Contains("wall"))
+                        inp = "wall";
+                    else if (inp.Contains("left"))
+                        inp = "left";
+                    else if (inp.Contains("right"))
+                        inp = "right";
+                    else if (inp.Contains("up"))
+                        inp = "up";
+                    else if (inp.Contains("down"))
+                        inp = "down";
+                }
+                else
+                {
+
+                    // Acceptable inputs
+                    switch (inp.ToLower())
+                    {   
+                        case "l":
+                        case "le":
+                        case "lef":
+                        case "left":
+                        case "move l":
+                        case "move le":
+                        case "move lef":
+                        case "move left":
+                            inp = "left";
+                            break;
+                        case "r":
+                        case "ri":
+                        case "rig":
+                        case "righ":
+                        case "right":
+                        case "move r":
+                        case "move ri":
+                        case "move rig":
+                        case "move righ":
+                        case "move right":
+                            inp = "right";
+                            break;
+                        case "u":
+                        case "up":
+                        case "move u":
+                        case "move up":
+                            inp = "up";
+                            break;
+                        case "d":
+                        case "do":
+                        case "dow":
+                        case "down":
+                        case "move d":
+                        case "move do":
+                        case "move dow":
+                        case "move down":
+                            inp = "down";
+                            break;
+                        case "w":
+                        case "wa":
+                        case "wal":
+                        case "wall":
+                        case "p":
+                        case "pl":
+                        case "pla":
+                        case "plac":
+                        case "place":
+                        case "place w":
+                        case "place wa":
+                        case "place wal":
+                        case "place wall":
+                            inp = "wall";
+                            break;
+                        default:
+                            write("Enter one of the given options:");
+                            continue;
+                    }
+
+                }
                 char optionType = Player.validateOption(options, inp);
 
                 if (optionType == 'b')
